d92085230fc882f618ecc292dede21299d6c8e7a
jkwang <wjkcow@umich.edu>
2017-12-09 20:18:59 -0800

    update test

--- test/test2/on_start.sh
+++ test/test2/on_start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ramble-distribution
+cd /usr/src/ramble/ramble-distribution
 ./bin/ramble-cli-setup-keys.sh
 ./bin/ramble-cli-setup-db.sh 5000 5001
 
@@ -8,10 +8,12 @@ cd ramble-distribution
 file=/testdata/start.sh
 tc qdisc add dev eth0 root netem delay 20ms
 
-while [ ! -f "$file" ]
-do
-    inotifywait -qqt 2 -e create -e moved_to "$(dirname $file)"
-done
+./bin/ramble-cli.sh -f /testroot/testresult/ramble-messages-$HOSTNAME.txt -pu /tmp/ramble-key-store/ramble-cli.pub -pr /tmp/ramble-key-store/ramble-cli -p udp://172.20.128.1:5000
 
-/testdata/start.sh
+#while [ ! -f "$file" ]
+#do
+#    inotifywait -qqt 2 -e create -e moved_to "$(dirname $file)"
+#done
+
+#/testdata/start.sh
 
--- test/test2/run_test.sh
+++ test/test2/run_test.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-sudo docker build -t test0 .
-
-tc qdisc add dev eth0 root netem delay 20ms
+#tc qdisc add dev eth0 root netem delay 20ms
 
 for number in {1..10}
 do
 #sudo docker run --net testnet --ip 172.20.128.$number  -it --rm -v $(pwd)/testresult:/testdata test0  sh 
-sudo docker run --net testnet --ip 172.20.128.$number  -it --rm -v $(pwd)/testresult:/testdata --cap-add=NET_ADMIN -d=true test0 # sh 
+sudo docker run --net testnet --ip 172.20.128.$number  -it --rm -v $(pwd):/testroot --cap-add=NET_ADMIN -d=true ramble /testroot/on_start.sh
 done
 
