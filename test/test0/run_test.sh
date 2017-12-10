74bb8d028bd2e8e42ce3c2fa0efb61cacd003989
jkwang <wjkcow@umich.edu>
2017-12-09 23:31:45 -0800

    update test0/1

--- test/test0/run_test.sh
+++ test/test0/run_test.sh
@@ -2,9 +2,9 @@
 
 sudo docker build -t test0 .
 
-for number in {10..20}
+for number in {1..10}
 do
 #sudo docker run --net testnet --ip 172.20.128.$number  -it --rm -v $(pwd)/testresult:/testdata test0  sh 
-sudo docker run --net testnet --ip 172.20.128.$number  -it --rm -v $(pwd)/testresult:/testdata -d=true test0 # sh 
+sudo docker run --net testnet --ip 172.20.128.$number --cap-add=NET_ADMIN  -it --rm -v $(pwd)/testresult:/testdata -d=true test0 # sh 
 done
 
