468ed39eb6902dc739f284c43cc850a981aaf043
jkwang <wjkcow@umich.edu>
2017-12-11 05:24:12 +0000

    change fanout to 2

--- core/src/main/java/ramble/core/RambleImpl.java
+++ core/src/main/java/ramble/core/RambleImpl.java
@@ -36,7 +36,7 @@ public class RambleImpl implements Ramble {
 
   private static final Logger LOG = Logger.getLogger(RambleImpl.class);
 
-  private static final int MESSAGE_BROADCAST_FANOUT = 4;
+  private static final int MESSAGE_BROADCAST_FANOUT = 2;
 
   private final MembershipService membershipService;
   private final PublicKey publicKey;
